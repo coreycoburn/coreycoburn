1f06b86bbb260c4742a04170157f30f3ff07a2cf
Corey Coburn <corey@coburn.codes>
2018-04-26 23:07:25 -0700

    Created contact page

--- layouts/partials/Footer.vue
+++ layouts/partials/Footer.vue
@@ -4,13 +4,13 @@
             <p class="mb-8 text-grey-darkest text-lg font-bold">Say hi @ <a :href="`mailto:${me.email}`" class="text-grey-darkest">{{ me.email }}</a></p>
             <ul class="list-reset flex justify-center mb-4">
                 <li v-animate-css.hover="'tada'" class="mr-4">
-                    <a href="https://twitter.com/coreycoburn" class="border-0"><icon name="brands/twitter"/></a>
+                    <a :href="social.twitter" class="border-0"><icon name="brands/twitter"/></a>
                 </li>
                 <li v-animate-css.hover="'tada'" class="mr-4">
-                    <a href="https://github.com/coreycoburn" class="border-0"><icon name="brands/github"/></a>
+                    <a :href="social.github" class="border-0"><icon name="brands/github"/></a>
                 </li>
                 <li v-animate-css.hover="'tada'">
-                    <a href="https://www.linkedin.com/in/coreycoburn" class="border-0"><icon name="brands/linkedin-in"/></a>
+                    <a href="social.linkedin" class="border-0"><icon name="brands/linkedin-in"/></a>
                 </li>
             </ul>
             <p>&copy; {{ year }}. All rights reserved.</p>
@@ -19,12 +19,13 @@
 </template>
 
 <script>
-import { me } from '@/config/content'
+import { me, social } from '@/config/content'
 
 export default {
     data() {
         return {
             me,
+            social,
             year: new Date().getFullYear()
         }
     }
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,21 +1,62 @@
 <template>
     <div>
         <div class="container mx-auto">
-            <h1>{{ title }}</h1>
+            <h1 class="text-center">{{ title }}</h1>
+            <div class="flex flex-col sm:flex-row justify-around mb-16">
+                <div class="flex-1 text-center">
+                    <icon name="solid/map-marker-alt" class="icon-md mb-3"/>
+                    <h3 class="mb-3">Location:</h3>
+                    <div class="leading-loose">San Francisco Bay Area</div>
+                    <div class="leading-loose">Local &amp; Remote Freelancer</div>
+                </div>
+                <div class="flex-1 text-center mt-6 sm:mt-0">
+                    <icon name="solid/envelope" class="icon-md mb-3"/>
+                    <h3 class="mb-3">Contact:</h3>
+                    <div class="leading-loose"><a :href="`mailto:${me.email}`">{{ me.email }}</a></div>
+                </div>
+                <div class="flex-1 text-center mt-6 sm:mt-0">
+                    <icon name="solid/bullhorn" class="icon-md mb-3"/>
+                    <h3 class="mb-3">Social:</h3>
+                    <ul class="flex list-reset justify-center">
+                        <li class="leading-loose mr-3" v-animate-css.hover="'tada'">
+                            <a :href="social.twitter" class="border-0"><icon name="brands/twitter"/></a>
+                        </li>
+                        <li class="leading-loose mr-3" v-animate-css.hover="'tada'">
+                            <a :href="social.github" class="border-0"><icon name="brands/github"/></a>
+                        </li>
+                        <li class="leading-loose" v-animate-css.hover="'tada'">
+                            <a :href="social.linkedin" class="border-0"><icon name="brands/linkedin-in"/></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <form-contact/>
         </div>
     </div>
 </template>
 
 <script>
+import { me, social } from '@/config/content'
+import FormContact from '@/forms/Contact'
+import '@/components/icons/solid/map-marker-alt'
+import '@/components/icons/solid/envelope'
+import '@/components/icons/solid/bullhorn'
+
 export default {
     head() {
         return {
             title: this.title
         }
     },
+    components: {
+        FormContact
+    },
     data() {
         return {
-            title: 'Contact'
+            title: 'Contact',
+            me,
+            social
         }
     }
 }
