74f9f6557248c110472a9587477041b4d703bd97
Corey Coburn <corey@coburn.codes>
2019-02-05 09:13:34 -0800

    Added error page

--- config/links.js
+++ config/links.js
@@ -1,7 +1,7 @@
 export const links = [
     { path: '/', text: 'Home' },
     { absolute: 'https://resume.coburn.codes', text: 'Resume' },
-    { path: 'services', text: 'Services' },
-    { path: 'portfolio', text: 'Portfolio' },
-    { path: 'contact', text: 'Contact' }
+    { path: '/about', text: 'About' },
+    { path: '/portfolio', text: 'Portfolio' },
+    { path: '/contact', text: 'Contact' }
 ]
---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mx-auto text-center mt-16 max-w-md">
+    <h1 class="text-5xl">{{ error.statusCode }} Error</h1>
+    <div class="text-lg">
+      <p>{{ error.message }}.</p>
+      <p>If you feel you've reached this page in error, please contact me @ <a :href="`mailto:${me.email}`" v-text="me.email"/>.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// Data
+import { me } from '@/config/content'
+
+export default {
+  props: ['error'],
+  data() {
+    return {
+      me
+    }
+  }
+}
+</script>
