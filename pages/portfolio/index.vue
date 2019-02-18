9dc699d348c1787071650a65b5ead37b056de96d
Corey Coburn <corey@coburn.codes>
2019-02-18 00:07:02 -0800

    Updated portfolio grid and improved UX

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -8,49 +8,52 @@
 
     <div class="mx-auto max-w-md text-center mb-16 text-xl px-6 md:px-0">
       <p>
-        A small sample of some of my work from over the past few years. To see some code examples, please visit my <a :href="social.github">
+        A small sample of some of my work from over the past few years. To see some code examples (including the code for this website), please visit my <a :href="social.github">
           Github page
         </a>.
       </p>
     </div>
 
-    <div class="flex flex-wrap">
-      <PortfolioGrid
-        image="cpa-valet-grid.jpg"
-        path="cpa-valet"
-        title="Marketing Site"
-        description="C.P.A. Valet"
-        class="border-r-4"
-      />
+    <section class="section">
+      <div class="grid mb-16">
+        <Portfolio
+          size="large"
+          image="cpa-valet-grid.jpg"
+          path="cpa-valet"
+        >
+          Marketing Site for C.P.A. Valet
+        </Portfolio>
 
-      <PortfolioGrid
-        image="cpa-valet-crm-grid.jpg"
-        path="cpa-valet-crm"
-        title="CRM Web Application"
-        description="C.P.A. Valet"
-        class="md:border-r-4"
-      />
+        <Portfolio
+          size="large"
+          image="cpa-valet-crm-grid.jpg"
+          path="cpa-valet-crm"
+        >
+          Web App (Custom CRM) for C.P.A. Valet
+        </Portfolio>
 
-      <PortfolioGrid
-        image="liv-grid.jpg"
-        path="liv-admin-template"
-        title="Admin template"
-        description="Liv Themes"
-      />
-    </div>
+        <Portfolio
+          size="large"
+          image="liv-grid.jpg"
+          path="liv-admin-template"
+        >
+          Admin template - Liv Themes
+        </Portfolio>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 // Components
-import PortfolioGrid from '@/components/PortfolioGrid'
+import Portfolio from '@/components/Portfolio'
 
 // Content
 import { social } from '@/config/content'
 
 export default {
   components: {
-    PortfolioGrid
+    Portfolio
   },
 
   head () {
@@ -67,3 +70,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.section {
+  @apply m-8 hidden;
+
+  @supports(display: grid) {
+    @apply block;
+  }
+}
+
+.grid {
+  display: grid;
+  grid-gap: 30px;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-auto-rows: 150px;
+  grid-auto-flow: row dense;
+}
+</style>
